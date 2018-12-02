b1cd2384765c1acc32ffc99d26be1abfdb39737e
Ade <olaadapo@users.noreply.github.com>
2018-12-02 14:55:14 -0600

    Update scrape-news-articles.py
    simplified process to load pages (not using Selenium driver);
    modified to adjust to changes on Quartz web page

--- scrape-news-articles.py
+++ scrape-news-articles.py
@@ -10,29 +10,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
-time.sleep(20)
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
-driver.get('https://qz.com/africa/')
-time.sleep(20)
+page = requests.get('https://qz.com/africa/latest')
 
-elements = driver.find_elements_by_xpath("//a[@class='latest header-button']")
-element = elements[0]
-
-element.click()
-time.sleep(5)
-
-page = driver.page_source
-driver.quit()
-
-soup = BeautifulSoup(page, 'html.parser')
-articles = soup.find(id='queue')
-weblinks = articles.find_all('h2', {'class':'queue-article-title'})
+soup = BeautifulSoup(page.content, 'html.parser')
+weblinks = soup.find_all('article')
 
 pagelinks = []
-for link in weblinks:
-    url = link.contents[0]
-    pagelinks.append(url.get('href'))
+for link in weblinks[5:]:
+    url = link.contents[0].find_all('a')[0]
+    pagelinks.append('http://qz.com'+url.get('href'))
 
 authorname = []
 title = []
@@ -48,46 +38,47 @@ for link in pagelinks:
     soup = BeautifulSoup(page.text, 'html.parser')    
     # get author name, if there's a named author
     try:
-        abody = soup.find(class_='author-name')
+        abody = soup.find(class_='d3284 africa').find('a')
         aname = abody.get_text() 
     except:
         aname = 'Anonymous'    
+
     # get article title
-    atitle = soup.find(itemprop='headline')
-    thetitle = atitle.get_text()    
+    atitle = soup.find(class_="_21349 africa none _4ca8e")
+    thetitle = atitle.get_text() 
     # get main article page
-    articlebody = soup.find(class_='item-body')
+    articlebody = soup.find(class_='_61c55')
     # get text
-    articletext = soup.find_all('p')
+    articletext = soup.find_all('p')[8:]
     # print text
-    for paragraph in articletext:
+    for paragraph in articletext[:-1]:
         # get the text only
         text = paragraph.get_text()
-        paragraphtext.append(text)
+        paragraphtext.append(text)        
     # combine all paragraphs into an article
     thearticle.append(paragraphtext)
     authorname.append(aname)
     title.append(thetitle)
 
+# join paragraphs to re-create the article
 myarticle = [' '.join(article) for article in thearticle]
 
-import numpy as np
-from datetime import datetime
-
+# save article data to file
 data = {'Title':title, 
         'Author':authorname, 
         'PageLink':pagelinks, 
         'Article':myarticle, 
         'Date':datetime.now()}
 
-import pandas as pd
 oldnews = pd.read_excel('quartz\\news.xlsx')
 news = pd.DataFrame(data=data)
 cols = ['Title', 'Author', 'PageLink', 'Article', 'Date']
 news = news[cols]
 
 afronews = oldnews.append(news)
-afronews.drop_duplicates(subset='Title', inplace=True)
+afronews.drop_duplicates(subset='Title', keep='last', inplace=True)
+afronews.reset_index(inplace=True)
+afronews.drop(labels='index', axis=1, inplace=True)
 
 filename = 'quartz\\news.xlsx'
 wks_name = 'Data'
